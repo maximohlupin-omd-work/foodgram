27f878b4e58f70413520679b3d44e766c3410ff0
Максим Охлупин <maximohlupin.omd@yandex.ru>
2023-03-10 22:24:55 +0300

    add tag urls

---
+++ backend/tags/urls.py
@@ -0,0 +1,18 @@
+"""
+
+    @ File: urls.py
+    @ Date: 10.03.2023
+    @ Author: Ohlupin Maxim
+
+"""
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+router = DefaultRouter()
+
+router.register("tags", views.TagViewSet)
+
+urlpatterns = [
+    *router.urls
+]
